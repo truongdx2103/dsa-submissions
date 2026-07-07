400d6df84e1b6a3dac633b1460a96be22ccc6947
Truong Xuan Doan <truongdx2103@gmail.com>
2026-07-07 14:00:31 +0700

    Add: sentence-similarity - submission-0

---
+++ Data Structures & Algorithms/sentence-similarity/submission-0.py
@@ -0,0 +1,5 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2):
+            return False
+        return True
